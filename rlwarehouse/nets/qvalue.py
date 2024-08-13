b9550a6bc4600813583a1fc6bdb70dd16088f334
Ugurcan Ozalp <uurcann94@gmail.com>
2024-08-13 21:29:25 +0300

    dropout for qvalue network implemented with default zero dropout

--- rlwarehouse/nets/qvalue.py
+++ rlwarehouse/nets/qvalue.py
@@ -5,11 +5,11 @@ from .heads import *
 
         
 class ContinuousMLPQValue(nn.Module):
-    def __init__(self, observation_shape, action_shape, **kwargs):
+    def __init__(self, observation_shape, action_shape, dropout=0, **kwargs):
         super(ContinuousMLPQValue, self).__init__()
         num_inputs = observation_shape[0] + action_shape[0]
-        self.fc1 = nn.Sequential(nn.Linear(num_inputs, 256), nn.LayerNorm(256), nn.ReLU())
-        self.fc2 = nn.Sequential(nn.Linear(256, 256), nn.LayerNorm(256), nn.ReLU())
+        self.fc1 = nn.Sequential(nn.Linear(num_inputs, 256), nn.Dropout(dropout), nn.LayerNorm(256), nn.ReLU())
+        self.fc2 = nn.Sequential(nn.Linear(256, 256), nn.Dropout(dropout), nn.LayerNorm(256), nn.ReLU())
         self.q_fc = nn.Linear(256, 1)
         
     def forward(self, observation, action):
